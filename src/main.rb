8af855bfd1be8e8d5d0a76aad69840a9ecd083a4
mpk <niboshiporipori@gmail.com>
2014-11-15 23:53:36 +0900

    JSONの読み込み処理を1行に変更

--- src/main.rb
+++ src/main.rb
@@ -2,9 +2,6 @@ require_relative 'naver-crawler'
 require 'json'
 
 json = []
-open("site.json") do |io|
-	json = JSON.load(io) #=> [1, 2, 3, {"foo"=>"bar"}]
-end
-
+open("site.json"){ |io| json = JSON.load(io) }
 crawler = Crawler.new("./img", json["naver"])
 crawler.get_images("http://matome.naver.jp/odai/2140544391008706001")
