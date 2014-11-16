2d8f3da468f13ddc97187120e7f96badb990416f
mpk <niboshiporipori@gmail.com>
2014-11-16 18:45:17 +0900

    cssセレクタを記述したjsonファイルを読み込んだオブジェクトのキーをシンボルに変更 ついでに複数の引数に対応

--- src/main.rb
+++ src/main.rb
@@ -1,8 +1,11 @@
 require_relative 'crawler'
 require 'json'
 
-json = []
+SITE_JSON_NAME = "site.json"
 setting = JSON.parse( File.open("YMCrawlfile").read, {:symbolize_names => true} )
-open(setting[:site_json]){ |io| json = JSON.load(io) }
-crawler = Crawler.new(setting[:dst_dir], json["naver"], setting[:wait_time])
-crawler.save_images(ARGV[0])
+json_file_pass = FileTest.exist?(SITE_JSON_NAME) ? SITE_JSON_NAME : setting[:site_json]
+puts "reading site json file from #{json_file_pass}"
+json = JSON.parse( open(json_file_pass).read, {:symbolize_names => true} )
+File.write( SITE_JSON_NAME, JSON.unparse(json) ) unless FileTest.exist?(SITE_JSON_NAME)
+crawler = Crawler.new(setting[:dst_dir], json[:naver], setting[:wait_time])
+ARGV.each{ |v| crawler.save_images(v) }
